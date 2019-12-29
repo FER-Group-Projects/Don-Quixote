fddc9c70fb13c99c46861802fceac23b93cdb666
mlazaric <marko.lazaric@gmail.com>
2019-12-29 18:02:26 +0100

    Fix CI

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
-module don.quixote {
+open module don.quixote {
     requires javafx.controls;
     requires slf4j.api;
 
     exports hr.fer.zemris.projekt.gui;
+
+    // Testing requirements
+    requires java.desktop;
 }
