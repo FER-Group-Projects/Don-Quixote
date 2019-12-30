f9d7d197c7a3bd8dfa97192b9687f30f41e404ba
mlazaric <marko.lazaric@gmail.com>
2019-12-30 13:55:43 +0100

    Clean up genetic algorithm

--- src/main/java/hr/fer/zemris/projekt/algorithm/genetic/crossover/SinglePointCrossover.java
+++ src/main/java/hr/fer/zemris/projekt/algorithm/genetic/crossover/SinglePointCrossover.java
@@ -1,32 +1,29 @@
 package hr.fer.zemris.projekt.algorithm.genetic.crossover;
 
-import java.util.Random;
-
 import hr.fer.zemris.projekt.algorithm.solution.Solution;
 
+import java.util.Random;
 
-public class SinglePointCrossover implements GeneticCrossover<Solution<Double>> {
-	
-	private Random random = new Random();
 
-	@Override
-	public Solution<Double> crossover(Solution<Double> firstParent, Solution<Double> secondParent) {
-		
-		Solution<Double> child = firstParent.copy();
-		
-		int crossPoint = random.nextInt(firstParent.getNumberOfGenes());
-		
-		for(int i=0;i<firstParent.getNumberOfGenes();i++) {
-			
-			if(i<crossPoint) {
-				child.setGeneAt(i, firstParent.getGeneAt(i));
-			}
-			else {
-				child.setGeneAt(i, secondParent.getGeneAt(i));
-			}
-		}
-		
-		return child;
-	}
+public class SinglePointCrossover<G, S extends Solution<G>> implements GeneticCrossover<S> {
+
+    private Random random = new Random();
+
+    @Override
+    public S crossover(S firstParent, S secondParent) {
+        S child = (S) firstParent.copy();
+
+        int crossPoint = random.nextInt(firstParent.getNumberOfGenes());
+
+        for (int i = 0; i < firstParent.getNumberOfGenes(); i++) {
+            if (i < crossPoint) {
+                child.setGeneAt(i, firstParent.getGeneAt(i));
+            } else {
+                child.setGeneAt(i, secondParent.getGeneAt(i));
+            }
+        }
+
+        return child;
+    }
 
 }
--- src/main/java/hr/fer/zemris/projekt/algorithm/genetic/mutation/GaussMutation.java
+++ src/main/java/hr/fer/zemris/projekt/algorithm/genetic/mutation/GaussMutation.java
@@ -1,25 +1,27 @@
 package hr.fer.zemris.projekt.algorithm.genetic.mutation;
 
-import java.util.Random;
-
 import hr.fer.zemris.projekt.algorithm.solution.Solution;
 
+import java.util.Random;
+
 public class GaussMutation implements GeneticMutation<Solution<Double>> {
-	
-	private Random random = new Random();
-	private double gene;
 
-	@Override
-	public Solution<Double> mutate(Solution<Double> solutionToMutate) {
-		
-		for (int i = 0; i < solutionToMutate.getNumberOfGenes(); i++) {
-			
-			gene = solutionToMutate.getGeneAt(i) + random.nextGaussian();
-			solutionToMutate.setGeneAt(i, gene);
+    private Random random = new Random();
 
-		}
-		
-		return solutionToMutate;
-	}
+    private final double multiplier;
+
+    public GaussMutation(double multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    @Override
+    public Solution<Double> mutate(Solution<Double> solutionToMutate) {
+        for (int i = 0; i < solutionToMutate.getNumberOfGenes(); i++) {
+            double gene = solutionToMutate.getGeneAt(i) + multiplier * random.nextGaussian();
+            solutionToMutate.setGeneAt(i, gene);
+        }
+
+        return solutionToMutate;
+    }
 
 }
