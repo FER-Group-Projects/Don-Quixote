cce99f8bc5e94bef66f8f84fef29fe77e933ad40
Nikola Vnučec <nikola.vnucec@fer.hr>
2019-12-30 18:57:32 +0100

    Optimized feedforward neural network

--- src/main/java/hr/fer/zemris/projekt/feedforwardNeuralNet/NeuralNetwork.java
+++ src/main/java/hr/fer/zemris/projekt/feedforwardNeuralNet/NeuralNetwork.java
@@ -1,84 +1,75 @@
 package hr.fer.zemris.projekt.feedforwardNeuralNet;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class NeuralNetwork {
-
-    private Layer[] layers;
     private int[] layout;
 
-    public NeuralNetwork(int[] layout){
+    public NeuralNetwork(int[] layout) {
         this.layout = layout;
-        layers = new Layer[layout.length];
-        layers[0] = null;
-        for(int i=1; i<layout.length; i++){
-            layers[i] = new Layer(layout[i], layout[i-1]);
-        }
     }
 
-    public double[] calculateOutput(double[] inputs, double[] weights){
+    public double[] calculateOutput(double[] inputs, double[] weights) {
         checkArgs(inputs, weights);
-        layers[0] = new Layer(inputs);
 
-        setWeights(weights);
+        double[] layer_input = new double[inputs.length];
+        System.arraycopy(inputs, 0, layer_input, 0, inputs.length);
+        double[] layer_output = new double[layout[1]];
+        int weightIndex = 0;
 
-        double[] output = new double[layers[layers.length-1].listOfNeurons.length];
-        for(int i=1; i<layers.length; i++){
-            for(int j=0; j<layers[i].listOfNeurons.length; j++){
+        for (int i = 1; i < layout.length; i++) {
+            for (int j = 0; j < layout[i]; j++) {
+                double bias = weights[weightIndex];
+                weightIndex++;
+
                 double sum = 0;
-                for(int k=0; k<layers[i-1].listOfNeurons.length; k++){
-                    sum += layers[i-1].listOfNeurons[k].output * layers[i].listOfNeurons[j].inputWeights[k];
+                for (int k = 0; k < layer_input.length; k++) {
+                    sum += weights[weightIndex] * layer_input[k];
+                    weightIndex++;
                 }
-                sum += layers[i].listOfNeurons[j].bias;
-                layers[i].listOfNeurons[j].output = sigmoidFunction(sum);
-                if(i == layers.length-1){
-                    output[j] = layers[i].listOfNeurons[j].output;
-                }
+                sum += bias;
+                layer_output[j] = sigmoidFunction(sum);
             }
-        }
-        return output;
-    }
 
-    public void setWeights(double[] allWeights){
-        int index = 0;
-
-        for(int layer=1; layer<layers.length; layer++){
-            int numOfWeights = layers[layer-1].listOfNeurons.length;
-            for(int neuron=0; neuron<layers[layer].listOfNeurons.length; neuron++){
-                double[] weights = Arrays.copyOfRange(allWeights, index, index + numOfWeights);
-                index += numOfWeights + 1;
-                layers[layer].listOfNeurons[neuron].setInputWeights(weights, weights[numOfWeights - 1]);
+            if (i == layout.length - 1) {
+                break;
             }
+
+            layer_input = new double[layer_output.length];
+            System.arraycopy(layer_output, 0, layer_input, 0, layer_output.length);
+            layer_output = new double[layout[i + 1]];
         }
+
+        return layer_output;
     }
 
     //check that the args are valid
-    private void checkArgs(double[] inputs, double[] weights){
-        if (layout[0] != inputs.length){
+    private void checkArgs(double[] inputs, double[] weights) {
+        if (layout[0] != inputs.length) {
             throw new IllegalArgumentException("Invalid number  of input values! Expected: "
-                    +layout[0] + " Received " + inputs.length);
+                    + layout[0] + " Received " + inputs.length);
         }
 
         int num = getNumberOfWeights();
 
-        if(num!=weights.length){
+        if (num != weights.length) {
             throw new IllegalArgumentException("Invalid number  of weights! Expected: "
-                    +num+ " Recieved: " + weights.length);
+                    + num + " Recieved: " + weights.length);
         }
     }
 
     public int getNumberOfWeights() {
         int num = 0;
-        for (int i = 1; i < layout.length; i++){
+        for (int i = 1; i < layout.length; i++) {
             num += ((layout[i - 1] + 1) * layout[i]);
         }
         return num;
     }
 
+    public double getRandomDouble(double min, double max) {
+        return (Math.random() * ((max - min) + 1)) + min;
+    }
 
-    public double sigmoidFunction(double x){
-        return (double) (1/(1+Math.pow(Math.E, -x)));
+    public double sigmoidFunction(double x) {
+        return (double) (1 / (1 + Math.pow(Math.E, -x)));
     }
 
 }
